e77decfd63346668f51d5a038039a8dc4458db3e
AlexMc889 <amcso1000@yahoo.com>
2024-12-09 11:52:47 -0600

    Delete traffic_analysis.ps1

--- traffic_analysis.ps1
+++
@@ -1,5 +0,0 @@
-get-nettcpconnection | Where-Object { $_.State -eq "Established" } | 
-select-object -Property RemoteAddress | 
-Out-File -FilePath ".\listactiveips.txt"
-cat .\listactiveips.txt
-python .\iprepcheckerpowershell.py
